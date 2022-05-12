7f571c4aeb8b8156bc10032f67752217e7fbdbf9
Cosme Torandell Borrás <91748301+cosmetorandellborras@users.noreply.github.com>
2022-05-12 13:58:44 +0200

    Incorporamos clase CalculadoraTest y creamos los tests unitarios

---
+++ src/CalculadoraTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void testSuma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMultiplicacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivision() {
+		fail("Not yet implemented");
+	}
+
+}
